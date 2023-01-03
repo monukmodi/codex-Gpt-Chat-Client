5af3e4418a1f5c069c7915abcd16369d05f7e859
monukmodi <monukr2012@gmail.com>
2023-01-03 16:59:50 +0530

    text typing of every character after every interval

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ import user from './assets_chat_gpt/user.svg'
 const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
 
+// .... Loading
+
 let loadInterval
 
 function loader(element) {
@@ -16,4 +18,21 @@ function loader(element) {
       element.textContent = ''
     }
   }, 300)
+}
+
+// Typing every letter one by one
+
+function typeText(element, text) {
+  let index = 0
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      // If it is true it means we are still typing
+      element.innerHTML += text.charAt(index) // it will return a specific charater at index which AI is going to return
+      index++
+    } else {
+      // If we have reached to end of the text we will clear the interval
+      clearInterval(interval)
+    }
+  }, 20)
 }
