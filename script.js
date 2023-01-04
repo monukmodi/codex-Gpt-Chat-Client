67d8af4a5228e3312d915e39a2b63bff740ea1e2
monukmodi <monukr2012@gmail.com>
2023-01-04 11:37:59 +0530

    Changes in Client side

--- script.js
+++ script.js
@@ -85,6 +85,33 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId)
 
   loader(messageDiv)
+
+  // fetch data from server --- Bot's Response
+
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt'),
+    }),
+  })
+  clearInterval(loadInterval)
+  messageDiv.innerHTML = ''
+
+  if (response.ok) {
+    const data = await response.json()
+    const parsedData = data.bot.trim()
+
+    typeText(messageDiv, parsedData)
+  } else {
+    const err = await response.text()
+
+    messageDiv.innerHTML = 'Something went wrong'
+
+    alert(err)
+  }
 }
 
 form.addEventListener('submit', handleSubmit)
